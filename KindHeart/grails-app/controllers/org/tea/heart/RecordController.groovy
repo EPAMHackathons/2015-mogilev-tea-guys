5bae23294ed8a1a4b2bb6110c8a3aa860f1d8793
Dzianis Shybeka <dzianis_shybeka@epam.com>
2015-02-15 10:47:34 +0300

    Added feeder stub job

--- KindHeart/grails-app/controllers/org/tea/heart/RecordController.groovy
+++ KindHeart/grails-app/controllers/org/tea/heart/RecordController.groovy
@@ -1,6 +1,7 @@
 package org.tea.heart
 
 import grails.converters.JSON
+import org.springframework.social.twitter.api.GeoCode
 import org.springframework.social.twitter.api.SearchParameters
 import org.springframework.social.twitter.api.Twitter
 
@@ -11,10 +12,11 @@ class RecordController {
     def getAll() {
 
         SearchParameters searchParameters = new SearchParameters("#lost-pets")
-//        searchParameters.geoCode(GeoCode)
+        searchParameters.geoCode(GeoCode)
+//        searchParameters.
 
         def searchResults = twitter.searchOperations().search(searchParameters)
 
-        render "searchResults " + (searchResults as JSON);
+        render searchResults as JSON;
     }
 }
---
+++ KindHeart/grails-app/jobs/kindheart/FeederJob.groovy
@@ -0,0 +1,13 @@
+package kindheart
+
+
+
+class FeederJob {
+    static triggers = {
+      simple repeatInterval: 60000l // execute job once in 5 seconds
+    }
+
+    def execute() {
+        // execute job
+    }
+}
