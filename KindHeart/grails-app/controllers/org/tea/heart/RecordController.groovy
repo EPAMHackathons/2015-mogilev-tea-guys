778595aa056d3b7e96a68300bbbeff711ef82cf3
scadidi@gmail.com <scadidi@gmail.com>
2015-02-15 13:54:43 +0300

    added get by tag ids processing

--- KindHeart/grails-app/controllers/org/tea/heart/RecordController.groovy
+++ KindHeart/grails-app/controllers/org/tea/heart/RecordController.groovy
@@ -1,17 +1,18 @@
 package org.tea.heart
 
 import grails.converters.JSON
-//import org.springframework.social.twitter.api.GeoCode
 import org.springframework.social.twitter.api.SearchParameters
 import org.springframework.social.twitter.api.Twitter
 
 class RecordController {
 
     Twitter twitter
+    RecordProcessorService recordProcessorService
 
     def getAll() {
 
-        SearchParameters searchParameters = new SearchParameters("#ДобраеСэрца")
+        SearchParameters searchParameters = new SearchParameters("#ДОБРАЕСЭРЦА")
+
         //searchParameters.geoCode(GeoCode)
 //        searchParameters.
 
@@ -21,11 +22,12 @@ class RecordController {
 
             it.getEntities().each { hashTagEntity ->
 
-                hashTagEntity.getHashTags().each { hashTag ->
+                hashTagEntity.hashTags.each { hashTag ->
                     HashTag newTag = new HashTag()
                     newTag.name = hashTag.text
+
 //                    newTag.save()
-              }
+                }
             }
         }
 
@@ -45,7 +47,7 @@ class RecordController {
                 photoUrl:'',
                 hashTags: [ text:'ДобраеСэрца',
                             text:'Сабачка'],
-                location : 'Могиле',
+                location : 'Магилеу',
                 user:[  name: 'asd',
                         profileUrl:'http://twitter.com/AnimalAngel47',
                         profile_image_url: 'https://pbs.twimg.com/profile_images/560601273519865856/zVW7NNJX_normal.jpeg'],
@@ -56,6 +58,11 @@ class RecordController {
         results.add(answer)
         answer =  [success : false, model: model, message : 'test message']
         results.add(answer)
+        render results as JSON;
+    }
+
+    def getByTagIds(ids){
+        def results = recordProcessorService.searchByTags(ids)
         render results as JSON;
     }
 }
